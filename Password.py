451a910a211f810ec4ff2b54d1c2d796e6a58862
SophiaC42 <55824427+SophiaC42@users.noreply.github.com>
2019-12-09 15:01:17 -0800

    Add files via upload

---
+++ Password.py
@@ -0,0 +1,7 @@
+imageID = 'zWGAX18aUaBcDjkn2mN8duIE'
+imageSecret = 'DM1xZBhZJ4pPN7MowD2DQlnb9npmrqQTukNLw9qzVjAtyx4S'
+imageBase = 'https://api.everypixel.com/v1/keywords'
+
+microsoftKey = '60ce1b13702b401c8fce289b257dc150'
+microsoftBase = 'https://hcde318final.cognitiveservices.azure.com/'
+uri = 'hcde318final.cognitiveservices.azure.com'
